f4d8659afdfef0d8d8be68c3ac3a749a17abf5d5
Ivan <ivanolef.io@gmail.com>
2026-01-18 13:48:44 +0200

    Add new Notes

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,13 +1,10 @@
 import { Metadata } from 'next';
-// Змінюємо імпорт на локальний файл, який вимагає перевірка:
 import Notes from './Notes.client'; 
 import css from '@/components/NotesPage/NotesPage.module.css'
 
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
-
-// ... generateMetadata залишається без змін ...
 
 export default async function FilterPage({ params }: Props) {
   const { slug } = await params;
